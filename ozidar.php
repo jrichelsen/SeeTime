5913abe1c038a3beb28a37cb102028cd54f722ce
Jon Richelsen <jrichels@gmail.com>
2015-10-29 19:31:22 -0400

    initial commit

---
+++ ozidar.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Buck's Cool Tool Database</title>
+</head>
+<body>
+We can mix html with <?php echo '<b>PHP</b>' ?><br/>
+Today's date is <?=date("09/17/2015")?>
+</body>
+</html>
